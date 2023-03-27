24abed80b6c0af4744d7c1406f0c5c0583fd049c
Royi Elran <royi.elran@piiano.com>
2023-03-27 11:42:51 +0300

    merge with the latest version of vault-sample-python-django

--- examples/django-encryption-example/customers/models.py
+++ examples/django-encryption-example/customers/models.py
@@ -4,11 +4,11 @@ from django_encryption.fields import EncryptedCharField, EncryptedEmailField, En
 
 
 class Customer(models.Model):
-    name = EncryptedCharField()
-    email = EncryptedEmailField()
-    phone = EncryptedCharField()
+    name = EncryptedCharField(data_type_name='NAME')
+    email = EncryptedEmailField(data_type_name='EMAIL')
+    phone = EncryptedCharField(data_type_name='PHONE_NUMBER')
     address = EncryptedCharField(
-        encryption_type=EncryptionType.randomized, expiration_secs=10)
+        encryption_type=EncryptionType.randomized, expiration_secs=10, data_type_name='ADDRESS')
     ssn = EncryptedCharField(
-        encryption_type=EncryptionType.randomized)
-    dob = EncryptedDateField()
+        encryption_type=EncryptionType.randomized, data_type_name='SSN')
+    dob = EncryptedDateField(data_type_name='DATE_OF_BIRTH')
